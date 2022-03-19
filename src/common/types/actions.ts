68be5bb82053f76a5cb4221c9ea958e4467da491
Nathan Folsom <n8folsom@gmail.com>
2022-03-19 11:46:36 -0700

    generic reducer typings

---
+++ src/common/types/actions.ts
@@ -0,0 +1,21 @@
+export enum ActionType {
+  set,
+  add,
+  delete,
+  update
+}
+type Success = boolean | Promise<boolean>;
+type WithSuccess<T extends Record<string, any>> = T & { success: Success };
+export type BaseAction<T extends ActionType, U> = {
+  action: T,
+  data: WithSuccess<U>
+}
+export type SetAction<T> = BaseAction<ActionType.set, { value: T }>;
+export type AddAction<T> = BaseAction<ActionType.add, { addition: T }>;
+export type DeleteAction<T> = BaseAction<ActionType.delete, { deletion: T }>;
+export type UpdateAction<T> = BaseAction<ActionType.update, { update: T }>;
+export type Action<Set, Add, Update, Delete> =
+  | SetAction<Set>
+  | AddAction<Add>
+  | UpdateAction<Update>
+  | DeleteAction<Delete>;
